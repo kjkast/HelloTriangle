aae07adf95c2fd92d7459d22bcce21195f417e53
KJ <116606303+kjkast@users.noreply.github.com>
2024-12-05 10:53:21 -0500

    Delete triangle.cpp
    unneccessary file

--- triangle.cpp
+++
@@ -1,9 +0,0 @@
-// three vertices for a triangle
-float vertices[] = {
-	-0.5f, -0.5f, 0.0f, // z at 0 to make it look 2D (depth remains same)
-	0.5f, -0.5f, 0.0f,
-	0.0f, 0.5f, 0.0f
-};
-
-unsigned int VBO{};
-glGenBuffers(1, &VBO);
